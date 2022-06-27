67be5a2eee8f97de6d7af00104d651e4eef72315
xin.huang <xin.huang@china.zhaogang.com>
2022-06-27 16:22:32 +0800

    feat: node_env

--- .umirc.ts
+++ .umirc.ts
@@ -1,12 +1,12 @@
 import { defineConfig } from 'dumi';
 
-const { UMI_ENV } = process.env;
+const { NODE_ENV } = process.env;
 export default defineConfig({
   title: 'xin-ui',
   favicon:
     'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
   logo: 'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
   outputPath: 'docs-dist',
-  publicPath: UMI_ENV === 'production'  ? 'xin-ui' : '/'
+  publicPath: NODE_ENV === 'production' ? '/xin-ui' : '/',
   // more config: https://d.umijs.org/config
 });

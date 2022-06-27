62e9cb932644ebd4baffa345cecfd71b67ec4a10
xin.huang <xin.huang@china.zhaogang.com>
2022-06-27 16:40:15 +0800

    feat: umi config base

--- .umirc.ts
+++ .umirc.ts
@@ -7,6 +7,7 @@ export default defineConfig({
     'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
   logo: 'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
   outputPath: 'docs-dist',
-  publicPath: NODE_ENV === 'production' ? '/xin-ui/' : '/',
+  base: '/xin-ui/',
+  publicPath: NODE_ENV === 'production' ? '/xin-ui/' : '/xin-ui/',
   // more config: https://d.umijs.org/config
 });

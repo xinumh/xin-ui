28ea16cb90db4dba8cf90bbd5a15797c5c2842b5
xin.huang <xin.huang@china.zhaogang.com>
2022-07-15 11:41:10 +0800

    feat: Text

--- src/components/Text/index.tsx
+++ src/components/Text/index.tsx
@@ -2,25 +2,9 @@ import React from 'react';
 
 type Rainbow = 'red' | 'orange' | 'yellow' | 'green' | 'blue' | 'indigo' | 'violet';
 
-type ITextProps<C extends React.ElementType> = {
-  as?: C;
-  color?: Rainbow | 'black';
-};
-
 type AsProp<C extends React.ElementType> = {
   as?: C;
 };
-
-type TextProps = {
-  color?: Rainbow | 'black';
-};
-
-/**
- * C 根据 as 确定的元素类型
- * Props 组件的属性，eg：ItextProps
- */
-type Props<C extends React.ElementType> = React.PropsWithChildren<ITextProps<C>> &
-  Omit<React.ComponentPropsWithoutRef<C>, keyof ITextProps<C>>;
 
 type PropsToOmit<C extends React.ElementType, P> = keyof (AsProp<C> & P);
 type PolymorphicComponentProp<C extends React.ElementType, Props = {}> = React.PropsWithChildren<
@@ -28,26 +12,45 @@ type PolymorphicComponentProp<C extends React.ElementType, Props = {}> = React.P
 > &
   Omit<React.ComponentPropsWithoutRef<C>, PropsToOmit<C, Props>>;
 
-const Text = <C extends React.ElementType = 'span'>({
-  as,
-  color,
-  children,
-  ...restProps
-}: PolymorphicComponentProp<C, TextProps>) => {
-  const Component = as || 'span';
-  const style = color ? { style: { color } } : {};
-  return (
-    <Component {...restProps} {...style}>
-      {children}
-    </Component>
-  );
-};
+type PolymorphicComponentPropWithRef<
+  C extends React.ElementType,
+  Props = {},
+> = PolymorphicComponentProp<C, Props> & { ref?: PolymorphicRef<C> };
 
-const Text2 = () => {
+type TextProps<C extends React.ElementType> = PolymorphicComponentPropWithRef<
+  C,
+  { color?: Rainbow | 'black' }
+>;
+
+type PolymorphicRef<C extends React.ElementType> = React.ComponentPropsWithRef<C>['ref'];
+
+type TextComponent = <C extends React.ElementType = 'span'>(
+  props: TextProps<C>,
+) => React.ReactElement | null;
+
+const Text: TextComponent = React.forwardRef(
+  <C extends React.ElementType>(
+    { as, color, children, ...restProps }: TextProps<C>,
+    ref?: PolymorphicRef<C>,
+  ) => {
+    const Component = as || 'span';
+    const style = color ? { style: { color } } : {};
+    return (
+      <Component {...restProps} {...style} ref={ref}>
+        {children}
+      </Component>
+    );
+  },
+);
+
+// const buttonRef = React.useRef<HTMLButtonElement>(null);
+const TextDemo = () => {
+  const divRef = React.useRef<HTMLDivElement>(null);
+
   return (
-    <>
-      <Text color="blue">Hello Polymorphic!</Text>
-    </>
+    <Text as="div" color="blue" ref={divRef}>
+      Hello Polymorphic!
+    </Text>
   );
 };
-export default Text2;
+export default TextDemo;
